05cfd8a406e3348dfe6d953a9afafef64dce7267
Vadim Fedorov <vadim.fedorov@gmail.com>
2024-03-21 11:57:04 +0200

    Updated setup.py

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ with open(this_dir / "README.md", encoding="utf-8") as f:
 
 
 setup(
-    name='pyscbwrapper',
+    name='pycspwrapper',
     version='0.1.1',
-    description="Python wrapper for Statistics Sweden's API, which can be found at http://www.scb.se/en/api.",
+    description="Python wrapper for Latvian official statistics portal API: https://stat.gov.lv/en/api-and-code-list-registry.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/kirajcg/pyscbwrapper',
-    author='Kira Coder Gylling',
-    author_email='kira.gylling@gmail.com',
+    url='https://github.com/vf42/pycspwrapper',
+    author='Vadim Fedorov',
+    author_email='vadim@vf42.com',
     license='MIT',
     classifiers=[
         "License :: OSI Approved :: MIT License",
